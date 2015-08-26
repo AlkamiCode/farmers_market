f28b5d34442d2c63d375a0b5eaf2c03e48d96885
mitchashby16 <mitchashby16@gmail.com>
2015-08-25 18:43:40 -0600

    changed column photos in store table to photos_url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826002716) do
+ActiveRecord::Schema.define(version: 20150826004129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,7 +89,7 @@ ActiveRecord::Schema.define(version: 20150826002716) do
   create_table "stores", force: :cascade do |t|
     t.string  "farm_name"
     t.string  "facebook_url"
-    t.string  "photo"
+    t.string  "photo_url"
     t.string  "twitter_url"
     t.string  "instagram_url"
     t.string  "description"
