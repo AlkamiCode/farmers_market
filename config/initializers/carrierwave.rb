eafba77f8f2a5d26c962ba47959a7a1ffa34906e
mitchashby16 <mitchashby16@gmail.com>
2015-09-02 17:38:46 -0600

    fixed secret keys for figaro

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ CarrierWave.configure do |config|
 
     config.fog_credentials = {
       provider:                         'Google',
-      #replace with figaro methods use find in project to replace all
+
       google_storage_access_key_id:     Figaro.env.google_storage_access_key_id,
       google_storage_secret_access_key: Figaro.env.google_storage_secret_access_key
     }
